b8eab4c5665d27fb0cd787f3335a21ad9a06aa62
giancarlit0 <giancarlomejiasantos2012@gmail.com>
2024-04-21 18:47:07 -0500

    pc2

--- src/main/java/dao/RevistaDAO.java
+++ src/main/java/dao/RevistaDAO.java
@@ -1,9 +1,11 @@
-package dao;
+ package dao;
+
+import java.util.List;
 
 import entity.Revista;
 
 public interface RevistaDAO {
 	
 	public abstract int insertaRevista(Revista obj); 
+	public abstract List<Revista> listaRevistaCompleja(String nombres,String frecuencia,int estado, int idModalidad);
 }
-
